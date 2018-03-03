ff241457c205407292c5e126ffc15b33744dc2ac
Shane Tidwell <shane.tidwell21@gmail.com>
2018-03-03 13:27:36 -0700

    finished project

--- src/components/WizardTwo/WizardTwo.js
+++ src/components/WizardTwo/WizardTwo.js
@@ -1,15 +1,18 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import {updateCity} from '../../ducks/reducer';
 
 class WizardTwo extends Component {
     render(){
+        const {updateCity} = this.props
         return(
             <div className="parent-div">
                 <div className="vert-align">
             
                 <p>In what city will the property be located?</p><br />
                     
-                        <input placeholder="city name" type="text" onChange={this.props.updateCity}/>
+                        <input placeholder="city name" type="text" onChange={(e)=>updateCity(e.target.value)}/>
                 
                     <Link to="/wThree"><button className="wTwo-btn"> Next </button></Link>
                 </div>
@@ -17,5 +20,11 @@ class WizardTwo extends Component {
         )
     }
 }
+function mapStateToProps(state){
+    return {
+        city: state.city
+    }
+    
+}
 
-export default WizardTwo;
+export default connect(mapStateToProps, {updateCity})(WizardTwo);
